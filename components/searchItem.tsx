abeefe23a655e3e81f195c2d554f67b21dfd966e
Maksymilian Dendura <maksd0410@gmail.com>
2022-05-21 16:09:25 +0200

    Added special color for already added cities

--- components/searchItem.tsx
+++ components/searchItem.tsx
@@ -12,22 +12,45 @@ const SearchItem = ({ city, onItemClick }: Props) => {
     cities.add(city);
     onItemClick();
   };
+  const cityAdded = () => {
+    let contains = false;
+    cities.cities.forEach((e) => {
+      if (e === city) contains = true;
+    });
+    return contains;
+  };
   return (
     <div
-      className="bg-white border-b-2 border-x-2 first-of-type:-mt-2 first-of-type:pt-3 last-of-type:rounded-b-lg py-1 px-2 cursor-pointer hover:bg-slate-100 relative group"
+      className={
+        cityAdded()
+          ? "bg-sky-800 text-white border-b-2 border-x-2 first-of-type:-mt-2 first-of-type:pt-3 last-of-type:rounded-b-lg py-1 px-2 cursor-pointer relative group"
+          : " bg-white  border-b-2 border-x-2 first-of-type:-mt-2 first-of-type:pt-3 last-of-type:rounded-b-lg py-1 px-2 cursor-pointer hover:bg-slate-100 relative group"
+      }
       onClick={handleClick}
     >
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        className="h-6 w-6 text-black absolute top-0 right-0 m-4 group-first-of-type:mt-6"
-        fill="none"
-        viewBox="0 0 24 24"
-        stroke="currentColor"
-        strokeWidth={2}
-      >
-        <path strokeLinecap="round" strokeLinejoin="round" d="M12 4v16m8-8H4" />
-      </svg>
-      {city.name} <br /> {city.coord.lon} {city.coord.lat}
+      {cityAdded() ? (
+        <></>
+      ) : (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-6 w-6 text-black absolute top-0 right-0 mx-4 mt-7 group-first-of-type:mt-8"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+          strokeWidth={2}
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M12 4v16m8-8H4"
+          />
+        </svg>
+      )}
+      {city.name}
+      <div className="text-sm">
+        Długość: {city.coord.lon.toString().slice(0, 5)}° <br />
+        Szerokość: {city.coord.lat.toString().slice(0, 5)}°
+      </div>
     </div>
   );
 };
