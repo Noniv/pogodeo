b8dfff67d8c6f98697ff5f262192381fc7265adf
Maksymilian Dendura <maksd0410@gmail.com>
2022-05-21 16:09:52 +0200

    Added footer and semantic HTML to layout

--- components/layout.tsx
+++ components/layout.tsx
@@ -4,11 +4,14 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <>
       <div className="h-screen flex flex-col">
-        <div className="bg-sky-800 w-full text-center text-xl font-bold text-white p-4 border-b-white border-b-2 sticky top-0 z-40">
-          Pogodeo
-        </div>
+        <header className="bg-sky-800 w-full text-center text-xl font-bold text-white p-4 border-b-white border-b-2 sticky top-0 z-40">
+          <h1>Pogodeo</h1>
+        </header>
         <div className="flex-grow">
         {children}
+        <footer className="bg-sky-800 w-full text-xl text-white p-4 border-b-white border-t-2 fixed bottom-0 z-40">
+          Realizacja: Maksymilian Dendura
+        </footer>
         </div>
       </div>
     </>
