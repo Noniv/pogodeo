3b75011fc46eb9c9788dbf282c3ec57e1b117b55
Maksymilian Dendura <maksd0410@gmail.com>
2022-05-22 16:13:08 +0200

    Added close icon to searchbar

--- components/searchbar.tsx
+++ components/searchbar.tsx
@@ -28,14 +28,14 @@ const SearchBar = () => {
     else setError("Nie znaleziono miasta.");
   };
 
-  const handleItemClick = () => {
+  const handleClose = () => {
     setCities([]);
     setError("");
     setSearchText("");
   };
 
   return (
-    <div className="w-4/5 max-w-5xl mx-auto relative z-10">
+    <div className="w-4/5 max-w-5xl mx-auto z-10 relative">
       <input
         type="text"
         className="block border-2 rounded-lg w-full px-2 py-2 relative z-10"
@@ -52,11 +52,28 @@ const SearchBar = () => {
         value={searchText}
         placeholder="Wpisz nazwę miasta..."
       />
+      {cities.length ? (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-7 w-7 absolute right-2 top-2 z-50 cursor-pointer"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+          strokeWidth={2}
+          onClick={handleClose}
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M6 18L18 6M6 6l12 12"
+          />
+        </svg>
+      ) : (
+        <></>
+      )}
       <div className="absolute w-full">
         {cities.map((city, i) => {
-          return (
-            <SearchItem key={i} city={city} onItemClick={handleItemClick} />
-          );
+          return <SearchItem key={i} city={city} onItemClick={handleClose} />;
         })}
       </div>
       <div className="text-red-500 h-6">{error}</div>
